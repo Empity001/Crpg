bcdd267d8c13943652302164a0fd7a7e47d22016
Empi_ty <srelmowow12@gmail.com>
2026-06-28 02:44:32 -0400

    Add files via upload

---
+++ bot/supabaseAdmin.js
@@ -0,0 +1,11 @@
+// bot/supabaseAdmin.js
+require('dotenv').config();
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseAdmin = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  { auth: { persistSession: false } }
+);
+
+module.exports = supabaseAdmin;
